3584348c637ac4d3f667290ab6f5c475b38f4ef2
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-01-21 23:07:08 +0000

    adding spinner and duck animation

---
+++ spinner1.js
@@ -0,0 +1,27 @@
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/    ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-    ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\    ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r𓅭    ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r🥚    ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r🐤    \n');
+}, 1600);
